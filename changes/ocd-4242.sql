8654b62fffd43b80a0a3cdad0833bb1c83a7c6a9
Todd Young <todd.young@pointclickcare.com>
2023-08-09 11:02:50 -0400

    feat!: drop test tool columns that are no longer used
    OCD-4242

---
+++ changes/ocd-4242.sql
@@ -0,0 +1,5 @@
+alter table openchpl.test_tool drop column if exists name;
+
+alter table openchpl.test_tool drop column if exists description;
+
+alter table openchpl.test_tool drop column if exists retired;
